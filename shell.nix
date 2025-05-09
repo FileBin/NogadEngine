4f87a350489af32bff6ef2ff99a708a5d5a5bd23
Filebin <alex.mousse9@gmail.com>
2025-05-10 00:57:55 +0300

    added eastl libraries to nix shell

--- shell.nix
+++ shell.nix
@@ -1,4 +1,60 @@
 with (import <nixpkgs> {  });
+let
+  EABase = pkgs.callPackage ({ stdenv, fetchFromGitHub }: 
+    stdenv.mkDerivation rec {
+      pname = "EABASE";
+      version = "123363eb82e132c0181ac53e43226d8ee76dea12";
+      
+      src = fetchFromGitHub {
+        owner = "electronicarts";
+        repo = "EABase";
+        rev = "${version}";
+        sha256 = "sha256-tQcGoOeyptMf/KQGC+o9P6XiTfJhi0xjwB2M/3JtnW4="; 
+      };
+
+      nativeBuildInputs = [ 
+        cmake
+      ];
+    }) {};
+  EASTL = pkgs.callPackage ({ stdenv, fetchFromGitHub }: 
+    stdenv.mkDerivation rec {
+      pname = "EASTL";
+      version = "3.21.23";
+      
+      nativeBuildInputs = [ 
+        cmake
+        pkg-config
+      ];
+
+      src = fetchFromGitHub {
+        owner = "electronicarts";
+        repo = "EASTL";
+        rev = "${version}";
+        sha256 = "sha256-8imixecWN/FOHY/9IxkIMbkxK7NXZ0TecZ4/SvOqf14="; 
+      };
+
+      cmakeFlags = [ 
+        "-DFETCHCONTENT_SOURCE_DIR_EABASE=${EABase.out}" 
+        "-DCMAKE_CXX_STANDARD_INCLUDE_DIRECTORIES=${EABase.out}/include"
+      ];
+
+      postInstall = ''
+        mkdir -p $out/lib/pkgconfig
+        cat > $out/lib/pkgconfig/eastl.pc <<EOF
+        prefix=$out
+        exec_prefix=\''${prefix}
+        libdir=\''${exec_prefix}/lib
+        includedir=\''${prefix}/include
+
+        Name: EASTL
+        Description: Electronic Arts Standard Template Library
+        Version: ${version}
+        Libs: -L\''${libdir} -lEASTL
+        Cflags: -I\''${includedir}
+        EOF
+      '';
+    }) {};
+in
 mkShell rec {
     buildInputs = [
         libgcc
@@ -20,7 +76,13 @@ mkShell rec {
         jq
         meson
         mesonlsp
+        SDL2
+        ninja
+        EASTL
+        cmake
+        pkg-config
     ];
+    
     packages = [
         (vscode-with-extensions.override {
             vscodeExtensions = with vscode-extensions; [
